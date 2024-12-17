b990a1c612722d3709c67983694cd478cbbafdd3
lakhdar <lakhdar.developpeur@gmail.com>
2024-12-17 18:40:21 +0100

    deployed

--- assets/react/controllers/Test.jsx
+++ assets/react/controllers/Test.jsx
@@ -247,6 +247,7 @@ export default function Test(props) {
 
             <div className="w-full text-center mb-10">
                 <h2 className=" text-xl font-bold leading-relaxed tracking-tight text-gray-700 md:text-2xl dark:text-white mb-10">Question n:° {count + 1}</h2>
+                <h2 className=" text-lg font-bold leading-relaxed tracking-tight text-gray-700 md:text-2xl dark:text-white mb-10">{result.type} </h2>
             </div>
             
  
@@ -260,18 +261,12 @@ export default function Test(props) {
                     </div> 
                  </div>
                 <div className="flex w-full">
-                    <div className="w-3/4 block lg:mr-3 sm:mb-4 lg:mb-1"> 
-                        <div>{result.type} </div>
-                        <div>{result.question}</div>
+                    <div className="w-3/4 block lg:mr-3 sm:mb-4 lg:mb-1 first-letter:uppercase"> 
+                    <h3 className=" text-lg font-bold leading-relaxed tracking-tight text-gray-700 md:text-2xl dark:text-white mb-10">{result.question}</h3>
                      </div>
                      <div className="w-1/4 items-center text-center">
                         <p className="text-4xl font-bold">{counter}/{seconde}</p> 
                      </div>
-                </div>
-                <div className="block w-full">
-                    <div className="w-full lg:mr-3 sm:mb-4 lg:mb-1">
-                       
-                    </div>
                 </div>
                 <div className="block w-full mb-5">
                     <div className="w-full lg:mr-3 sm:mb-4 lg:mb-1">
@@ -287,7 +282,7 @@ export default function Test(props) {
                 </div>
                 <div className="block w-full mb-5">
                     <div className="w-full lg:mr-3 sm:mb-4 lg:mb-1">
-                        <label htmlFor="Response_1" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Réponse 1</label>
+                        <label htmlFor="Response_1" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Réponse 2</label>
                         <div className="flex space-x-4">
                             
                             <div className="bg-gray-700/5 border border-gray-300 text-gray-700 text-sm rounded outline-none focus:ring-gray-900 focus:border-gray-700 block w-3/4 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-gray-500 dark:focus:border-gray-500">
@@ -300,7 +295,7 @@ export default function Test(props) {
                 </div>
                 <div className="block w-full mb-5">
                     <div className="w-full lg:mr-3 sm:mb-4 lg:mb-1">
-                        <label htmlFor="Response_1" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Réponse 1</label>
+                        <label htmlFor="Response_1" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Réponse 3</label>
                         <div className="flex space-x-4">
                             <div className="bg-gray-700/5 border border-gray-300 text-gray-700 text-sm rounded outline-none focus:ring-gray-900 focus:border-gray-700 block w-3/4 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-gray-500 dark:focus:border-gray-500">
                                 {result.response[2]}
