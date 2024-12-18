1ca05cf898ce9b352886752eace1ee4e2f91f080
lakhdar <lakhdar.developpeur@gmail.com>
2024-12-18 14:23:35 +0100

    add docker

--- assets/react/controllers/Response.jsx
+++ assets/react/controllers/Response.jsx
@@ -112,74 +112,69 @@ export default function Response(props) {
                 <h2 className=" text-xl font-bold leading-relaxed tracking-tight text-gray-700 md:text-2xl dark:text-white mb-4">Ajouter une question</h2>
             </div>
             
-            <form  className="mt-4 space-y-4 lg:mt-5 md:space-y-5" method="post">
-
-
             <div className="mb-4">
-                    <div className="w-full lg:mr-3 sm:mb-4 lg:mb-1">
-                        <label htmlFor="question_img" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white  w-full">
-                            <img ref={output} className="w-full border border-gray-700 rounded" id="output" src="https://placehold.co/600x400"/>
-                        </label>
-                        <input ref={image} id="question_img" type="file" className="hidden" onChange={handleChange.bind(event)} encType="multipart/form-data" multiple />
-                    </div> 
-                 </div>
-                <div className="block w-full">
-                    <div className="w-full lg:mr-3 sm:mb-4 lg:mb-1"> 
-                        <label htmlFor="type" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Type question</label>
-                        
-                       <select name="type" id="type" ref={type} className="bg-gray-700/5 border border-gray-300 text-gray-700 text-sm rounded outline-none focus:ring-gray-900 focus:border-gray-700 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-gray-500 dark:focus:border-gray-500">
-                            <option value="Les règles de circulation">Les règles de circulation</option>
-                            <option value="La sécurité routière">La sécurité routière</option>
-                            <option value="La mécanique et l’entretien">La mécanique et l’entretien</option>
-                            <option value="Les situations pratiques">Les situations pratiques</option>
-                            <option value="Les règles spécifiques">Les règles spécifiques</option>
-                            <option value="Le comportement du conducteur">Le comportement du conducteur</option>
-                       </select>
-                     </div>
-                </div>
-                <div className="block w-full">
-                    <div className="w-full lg:mr-3 sm:mb-4 lg:mb-1">
-                        <label htmlFor="question" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Question</label>
-                       <input id="question" type="text" ref={question} className="bg-gray-700/5 border border-gray-300 text-gray-700 text-sm rounded outline-none focus:ring-gray-900 focus:border-gray-700 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-gray-500 dark:focus:border-gray-500"/>
-                     </div>
-                </div>
-                <div className="block w-full mb-5">
-                    <div className="w-full lg:mr-3 sm:mb-4 lg:mb-1">
-                        <label htmlFor="Response_1" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Réponse 1</label>
-                        <div className="flex space-x-4">
-                            <input id="Response_1" type="text" ref={response_1} className="bg-gray-700/5 border border-gray-300 text-gray-700 text-sm rounded outline-none focus:ring-gray-900 focus:border-gray-700 block w-3/4 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-gray-500 dark:focus:border-gray-500"/>
-                            <Switcher className="w-1/4" sendDataToParent={handleFirst} />
-                        </div>
-                     
-                     </div>
-                </div>
-                <div className="block w-full mb-5">
-                    <div className="w-full lg:mr-3 sm:mb-4 lg:mb-1">
-                        <label htmlFor="Response_1" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Réponse 1</label>
-                        <div className="flex space-x-4">
-                            <input id="Response_1" type="text" ref={response_2} className="bg-gray-700/5 border border-gray-300 text-gray-700 text-sm rounded outline-none focus:ring-gray-900 focus:border-gray-700 block w-3/4 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-gray-500 dark:focus:border-gray-500"/>
-                            <Switcher className="w-1/4" sendDataToParent={handleSecond} />
-                        </div>
-                     
-                     </div>
-                </div>
-                <div className="block w-full mb-5">
-                    <div className="w-full lg:mr-3 sm:mb-4 lg:mb-1">
-                        <label htmlFor="Response_1" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Réponse 1</label>
-                        <div className="flex space-x-4">
-                            <input id="Response_1" type="text" ref={response_3} className="bg-gray-700/5 border border-gray-300 text-gray-700 text-sm rounded outline-none focus:ring-gray-900 focus:border-gray-700 block w-3/4 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-gray-500 dark:focus:border-gray-500"/>
-                            <Switcher className="w-1/4" sendDataToParent={handleThird} />
-                        </div>
-                     
-                     </div>
-                </div>
-                <div className="flex w-full justify-center">
-                    <button onClick={handleAjaxPut} type="submit" className=" text-base  text-white hover:bg-gray-900 bg-gray-700  dark:text-white dark:hover:bg-gray-700 font-medium rounded px-4 py-2.5 duration-300 transition-colors focus:outline-none">
-                        Envoyer
-                    </button>
-                </div>
-            
-            </form>
+                <div className="w-full lg:mr-3 sm:mb-4 lg:mb-1">
+                    <label htmlFor="question_img" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white  w-full">
+                        <img ref={output} className="w-full border border-gray-700 rounded" id="output" src="https://placehold.co/600x400"/>
+                    </label>
+                    <input ref={image} id="question_img" type="file" className="hidden" onChange={handleChange.bind(event)} encType="multipart/form-data" multiple />
+                </div> 
+            </div>
+            <div className="block w-full">
+                <div className="w-full lg:mr-3 sm:mb-4 lg:mb-1"> 
+                    <label htmlFor="type" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Type question</label>
+                    
+                    <select name="type" id="type" ref={type} className="bg-gray-700/5 border border-gray-300 text-gray-700 text-sm rounded outline-none focus:ring-gray-900 focus:border-gray-700 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-gray-500 dark:focus:border-gray-500">
+                        <option value="Les règles de circulation">Les règles de circulation</option>
+                        <option value="La sécurité routière">La sécurité routière</option>
+                        <option value="La mécanique et l’entretien">La mécanique et l’entretien</option>
+                        <option value="Les situations pratiques">Les situations pratiques</option>
+                        <option value="Les règles spécifiques">Les règles spécifiques</option>
+                        <option value="Le comportement du conducteur">Le comportement du conducteur</option>
+                    </select>
+                    </div>
+            </div>
+            <div className="block w-full">
+                <div className="w-full lg:mr-3 sm:mb-4 lg:mb-1">
+                    <label htmlFor="question" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Question</label>
+                    <input id="question" type="text" ref={question} className="bg-gray-700/5 border border-gray-300 text-gray-700 text-sm rounded outline-none focus:ring-gray-900 focus:border-gray-700 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-gray-500 dark:focus:border-gray-500"/>
+                    </div>
+            </div>
+            <div className="block w-full mb-5">
+                <div className="w-full lg:mr-3 sm:mb-4 lg:mb-1">
+                    <label htmlFor="Response_1" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Réponse 1</label>
+                    <div className="flex space-x-4">
+                        <input id="Response_1" type="text" ref={response_1} className="bg-gray-700/5 border border-gray-300 text-gray-700 text-sm rounded outline-none focus:ring-gray-900 focus:border-gray-700 block w-3/4 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-gray-500 dark:focus:border-gray-500"/>
+                        <Switcher className="w-1/4" sendDataToParent={handleFirst} />
+                    </div>
+                    
+                    </div>
+            </div>
+            <div className="block w-full mb-5">
+                <div className="w-full lg:mr-3 sm:mb-4 lg:mb-1">
+                    <label htmlFor="Response_1" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Réponse 2</label>
+                    <div className="flex space-x-4">
+                        <input id="Response_1" type="text" ref={response_2} className="bg-gray-700/5 border border-gray-300 text-gray-700 text-sm rounded outline-none focus:ring-gray-900 focus:border-gray-700 block w-3/4 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-gray-500 dark:focus:border-gray-500"/>
+                        <Switcher className="w-1/4" sendDataToParent={handleSecond} />
+                    </div>
+                    
+                    </div>
+            </div>
+            <div className="block w-full mb-5">
+                <div className="w-full lg:mr-3 sm:mb-4 lg:mb-1">
+                    <label htmlFor="Response_1" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Réponse 3</label>
+                    <div className="flex space-x-4">
+                        <input id="Response_1" type="text" ref={response_3} className="bg-gray-700/5 border border-gray-300 text-gray-700 text-sm rounded outline-none focus:ring-gray-900 focus:border-gray-700 block w-3/4 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-gray-500 dark:focus:border-gray-500"/>
+                        <Switcher className="w-1/4" sendDataToParent={handleThird} />
+                    </div>
+                    
+                    </div>
+            </div>
+            <div className="flex w-full justify-center">
+                <button onClick={handleAjaxPut} type="submit" className=" text-base  text-white hover:bg-gray-900 bg-gray-700  dark:text-white dark:hover:bg-gray-700 font-medium rounded px-4 py-2.5 duration-300 transition-colors focus:outline-none">
+                    Envoyer
+                </button>
+            </div>
         </div>
     </div>
 </section>
--- assets/react/controllers/UpdateQ.jsx
+++ assets/react/controllers/UpdateQ.jsx
@@ -137,9 +137,6 @@ export default function UpdateQ(props) {
             <div className="w-full text-center mb-10">
                 <h2 className=" text-xl font-bold leading-relaxed tracking-tight text-gray-700 md:text-2xl dark:text-white mb-10">Editer une question</h2>
             </div>
-            
-            <form  className="mt-4 space-y-4 lg:mt-5 md:space-y-5" method="post">
-
 
             <div className="mb-4">
                     <div className="w-full lg:mr-3 sm:mb-4 lg:mb-1">
@@ -148,64 +145,62 @@ export default function UpdateQ(props) {
                         </label>
                         <input ref={image} id="question_img" type="file" className="hidden"  onChange={handleChange.bind(event)}  encType="multipart/form-data" multiple />
                     </div> 
-                 </div>
-                <div className="block w-full">
-                    <div className="w-full lg:mr-3 sm:mb-4 lg:mb-1"> 
-                        <label htmlFor="type" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Type question</label>
-                        
-                       <select name="type" id="type" ref={type} className="bg-gray-700/5 border border-gray-300 text-gray-700 text-sm rounded outline-none focus:ring-gray-900 focus:border-gray-700 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-gray-500 dark:focus:border-gray-500">
-                            <option value="Les règles de circulation">Les règles de circulation</option>
-                            <option value="La sécurité routière">La sécurité routière</option>
-                            <option value="La mécanique et l’entretien">La mécanique et l’entretien</option>
-                            <option value="Les situations pratiques">Les situations pratiques</option>
-                            <option value="Les règles spécifiques">Les règles spécifiques</option>
-                            <option value="Le comportement du conducteur">Le comportement du conducteur</option>
-                       </select>
-                     </div>
-                </div>
-                <div className="block w-full">
-                    <div className="w-full lg:mr-3 sm:mb-4 lg:mb-1">
-                        <label htmlFor="question" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Question</label>
-                       <input id="question" type="text" ref={question}  className="bg-gray-700/5 border border-gray-300 text-gray-700 text-sm rounded outline-none focus:ring-gray-900 focus:border-gray-700 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-gray-500 dark:focus:border-gray-500"/>
-                     </div>
-                </div>
-                <div className="block w-full mb-5">
-                    <div className="w-full lg:mr-3 sm:mb-4 lg:mb-1">
-                        <label htmlFor="Response_1" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Réponse 1</label>
-                        <div className="flex space-x-4">
-                            <input id="Response_1" type="text" ref={response_1} className="bg-gray-700/5 border border-gray-300 text-gray-700 text-sm rounded outline-none focus:ring-gray-900 focus:border-gray-700 block w-3/4 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-gray-500 dark:focus:border-gray-500"/>
-                            <Checkbox className="w-1/4" sendDataToParent={handleFirst} _data={first} />
-                        </div>
-                     
-                     </div>
-                </div>
-                <div className="block w-full mb-5">
-                    <div className="w-full lg:mr-3 sm:mb-4 lg:mb-1">
-                        <label htmlFor="Response_1" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Réponse 1</label>
-                        <div className="flex space-x-4">
-                            <input id="Response_1" type="text" ref={response_2} className="bg-gray-700/5 border border-gray-300 text-gray-700 text-sm rounded outline-none focus:ring-gray-900 focus:border-gray-700 block w-3/4 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-gray-500 dark:focus:border-gray-500"/>
-                            <Checkbox className="w-1/4" sendDataToParent={handleSecond} _data={second}  />
-                        </div>
-                     
-                     </div>
-                </div>
-                <div className="block w-full mb-5">
-                    <div className="w-full lg:mr-3 sm:mb-4 lg:mb-1">
-                        <label htmlFor="Response_1" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Réponse 1</label>
-                        <div className="flex space-x-4">
-                            <input id="Response_1" type="text" ref={response_3} className="bg-gray-700/5 border border-gray-300 text-gray-700 text-sm rounded outline-none focus:ring-gray-900 focus:border-gray-700 block w-3/4 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-gray-500 dark:focus:border-gray-500"/>
-                            <Checkbox className="w-1/4" sendDataToParent={handleThird}  _data={third} />
-                        </div>
-                     
-                     </div>
-                </div>
-                <div className="flex w-full justify-center">
-                    <button onClick={handleAjaxPut} type="submit" className=" text-base  text-white hover:bg-gray-900 bg-gray-700  dark:text-white dark:hover:bg-gray-700 font-medium rounded px-4 py-2.5 duration-300 transition-colors focus:outline-none">
-                        Envoyer
-                    </button>
-                </div>
-            
-            </form>
+            </div>
+            <div className="block w-full">
+                <div className="w-full lg:mr-3 sm:mb-4 lg:mb-1"> 
+                    <label htmlFor="type" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Type question</label>
+                    
+                    <select name="type" id="type" ref={type} className="bg-gray-700/5 border border-gray-300 text-gray-700 text-sm rounded outline-none focus:ring-gray-900 focus:border-gray-700 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-gray-500 dark:focus:border-gray-500">
+                        <option value="Les règles de circulation">Les règles de circulation</option>
+                        <option value="La sécurité routière">La sécurité routière</option>
+                        <option value="La mécanique et l’entretien">La mécanique et l’entretien</option>
+                        <option value="Les situations pratiques">Les situations pratiques</option>
+                        <option value="Les règles spécifiques">Les règles spécifiques</option>
+                        <option value="Le comportement du conducteur">Le comportement du conducteur</option>
+                    </select>
+                    </div>
+            </div>
+            <div className="block w-full">
+                <div className="w-full lg:mr-3 sm:mb-4 lg:mb-1">
+                    <label htmlFor="question" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Question</label>
+                    <input id="question" type="text" ref={question}  className="bg-gray-700/5 border border-gray-300 text-gray-700 text-sm rounded outline-none focus:ring-gray-900 focus:border-gray-700 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-gray-500 dark:focus:border-gray-500"/>
+                    </div>
+            </div>
+            <div className="block w-full mb-5">
+                <div className="w-full lg:mr-3 sm:mb-4 lg:mb-1">
+                    <label htmlFor="Response_1" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Réponse 1</label>
+                    <div className="flex space-x-4">
+                        <input id="Response_1" type="text" ref={response_1} className="bg-gray-700/5 border border-gray-300 text-gray-700 text-sm rounded outline-none focus:ring-gray-900 focus:border-gray-700 block w-3/4 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-gray-500 dark:focus:border-gray-500"/>
+                        <Checkbox className="w-1/4" sendDataToParent={handleFirst} _data={first} />
+                    </div>
+                    
+                    </div>
+            </div>
+            <div className="block w-full mb-5">
+                <div className="w-full lg:mr-3 sm:mb-4 lg:mb-1">
+                    <label htmlFor="Response_1" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Réponse 1</label>
+                    <div className="flex space-x-4">
+                        <input id="Response_1" type="text" ref={response_2} className="bg-gray-700/5 border border-gray-300 text-gray-700 text-sm rounded outline-none focus:ring-gray-900 focus:border-gray-700 block w-3/4 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-gray-500 dark:focus:border-gray-500"/>
+                        <Checkbox className="w-1/4" sendDataToParent={handleSecond} _data={second}  />
+                    </div>
+                    
+                    </div>
+            </div>
+            <div className="block w-full mb-5">
+                <div className="w-full lg:mr-3 sm:mb-4 lg:mb-1">
+                    <label htmlFor="Response_1" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Réponse 1</label>
+                    <div className="flex space-x-4">
+                        <input id="Response_1" type="text" ref={response_3} className="bg-gray-700/5 border border-gray-300 text-gray-700 text-sm rounded outline-none focus:ring-gray-900 focus:border-gray-700 block w-3/4 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-gray-500 dark:focus:border-gray-500"/>
+                        <Checkbox className="w-1/4" sendDataToParent={handleThird}  _data={third} />
+                    </div>
+                    
+                    </div>
+            </div>
+            <div className="flex w-full justify-center">
+                <button onClick={handleAjaxPut} type="submit" className=" text-base  text-white hover:bg-gray-900 bg-gray-700  dark:text-white dark:hover:bg-gray-700 font-medium rounded px-4 py-2.5 duration-300 transition-colors focus:outline-none">
+                    Envoyer
+                </button>
+            </div>
         </div>
     </div>
 </section>
